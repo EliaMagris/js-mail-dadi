eae5985512380e9a68775fccc15a3b862a1c68f4
EliaMagris <elia.magris@libero.it>
2022-10-24 17:27:15 +0200

    esercizio dadi basic completato

--- assets/main.js
+++ assets/main.js
@@ -7,18 +7,38 @@ function mailVisual() {
     "haSconfittoMafia@hotmail.it",
   ];
 
-let emailTrue = false;
-  for (let i = 0; i < everyMail.length; i++) {
-  
-    if (inputMail == everyMail[i]) {
-      emailTrue = true; 
-    } 
-}
+  let emailTrue = false;
 
-    if(emailTrue == true){
-      document.getElementById("Risultato").innerHTML = `<div>BENVENUTO</div>` 
-    
-    }else{
-      document.getElementById("Risultato").innerHTML = `<div>REGISTRATI</div>`
+  for (let i = 0; i < everyMail.length; i++) {
+    if (inputMail == everyMail[i]) {
+      emailTrue = true;
     }
   }
+
+  if (emailTrue == true) {
+    document.getElementById("Risultato").innerHTML = `<div>BENVENUTO</div>`;
+  } else {
+    document.getElementById("Risultato").innerHTML = `<div>REGISTRATI</div>`;
+  }
+}
+
+// -------------------------------------------------------------
+
+function numberVisual() {
+  let giocatore = (document.getElementById("playerNumber").innerHTML = Math.round(Math.random() * 6));
+
+  let pc = (document.getElementById("pcNumber").innerHTML = Math.round(Math.random() * 6));
+
+  if (giocatore > pc) {
+    console.log("vince il giocatore");
+    document.getElementById("numberResult").innerHTML = `<div>VINCE IL GIOCATORE</div>`;
+  } else if (giocatore < pc) {
+    console.log("vince il PC");
+    document.getElementById("numberResult").innerHTML = `<div>VINCE IL PC</div>`;
+  } else {
+    console.log("pareggio");
+    document.getElementById("numberResult").innerHTML = `<div>BRUTTO STO PAREGGIO</div>`;
+  }
+}
+
+
