8a9a3effbf4fd274e80202dd39290f0f927a6f33
Elia Magris <elia.magris@libero.it>
2022-10-24 22:05:14 +0200

    esercizio completato

--- assets/main.js
+++ assets/main.js
@@ -24,6 +24,12 @@ function mailVisual() {
 
 // -------------------------------------------------------------
 
+let giocatoreWins = 0;
+let pcWins = 0;
+
+let userWin = document.getElementById("counterPlayer");
+let pcWin = document.getElementById("counterPC")
+
 function numberVisual() {
   let giocatore = (document.getElementById("playerNumber").innerHTML = Math.round(Math.random() * 6));
 
@@ -32,13 +38,21 @@ function numberVisual() {
   if (giocatore > pc) {
     console.log("vince il giocatore");
     document.getElementById("numberResult").innerHTML = `<div>VINCE IL GIOCATORE</div>`;
+    giocatoreWins = giocatoreWins + 1;
   } else if (giocatore < pc) {
     console.log("vince il PC");
     document.getElementById("numberResult").innerHTML = `<div>VINCE IL PC</div>`;
+    pcWins = pcWins + 1;
   } else {
     console.log("pareggio");
     document.getElementById("numberResult").innerHTML = `<div>BRUTTO STO PAREGGIO</div>`;
   }
+
+userWin.innerHTML = `Vittorie del giocatore: ${giocatoreWins}`;
+pcWin.innerHTML = `Vittorie del pc: ${pcWins}`;
+
 }
+
+
 
 
